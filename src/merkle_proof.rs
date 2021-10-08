ded50a7c25e6c004f2f3381450f5cca57656f363
Anton Suprunchuk <anton.suprunchuk@gmail.com>
2021-10-08 10:14:16 +0300

    feat: add a GitHub action to run tests on PRs

--- src/merkle_proof.rs
+++ src/merkle_proof.rs
@@ -87,7 +87,7 @@ impl<T: Hasher> MerkleProof<T> {
         let proof_indices_by_layers = utils::indices::proof_indices_by_layers(leaf_indices, total_leaves_count);
 
         // The next lines copy hashes from proof hashes and group them by layer index
-        let mut proof_layers: Vec<Vec<(usize, T::Hash)>> = Vec::with_capacity(tree_depth);
+        let mut proof_layers: Vec<Vec<(usize, T::Hash)>> = Vec::with_capacity(tree_depth + 1);
         let mut proof_copy = self.proof_hashes.clone();
         for proof_indices in proof_indices_by_layers {
             let proof_hashes = proof_copy.splice(0..proof_indices.len(), []);
